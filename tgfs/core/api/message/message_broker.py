9a2b0753a25f106c64e733be1a6a97514a9dac9f
Wheat Carrier <wheatcarrier@gmail.com>
2025-12-13 23:36:17 +0900

    retry with different bots when getting messages on error, and include the bot name in error log (#83)

--- tgfs/core/api/message/message_broker.py
+++ tgfs/core/api/message/message_broker.py
@@ -1,13 +1,19 @@
 import asyncio
+import logging
 from dataclasses import dataclass
 from functools import reduce
 from typing import List, Optional, Set
 
+from tgfs.config import get_config
 from tgfs.reqres import GetMessagesReq, GetMessagesResp, MessageResp
 from tgfs.telegram.interface import TDLibApi
 from tgfs.utils.message_cache import channel_cache
 
 DELAY = 0.5
+BOTS_COUNT = len(get_config().telegram.bot.tokens)
+
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -40,30 +46,51 @@ class MessageBroker:
         return await future
 
     async def process_requests(self):
-        try:
-            await asyncio.sleep(DELAY)
+        await asyncio.sleep(DELAY)
+        async with self.__lock:
+            requests, self.__requests = self.__requests, []
 
-            async with self.__lock:
-                requests, self.__requests = self.__requests, []
+        if not requests:
+            return
 
-            if not requests:
+        ids: Set[int] = reduce(lambda full, req: full.union(req.ids), requests, set())
+
+        e: Optional[Exception] = None
+        bot = self.tdlib.next_bot
+        for i in range(BOTS_COUNT):
+            try:
+
+                messages = await bot.get_messages(
+                    GetMessagesReq(
+                        chat=self.private_file_channel, message_ids=tuple(ids)
+                    )
+                )
+
+                messages_map = {
+                    msg.message_id: msg for msg in messages if msg is not None
+                }
+
+                for r in requests:
+                    if not r.future.done():
+                        r.future.set_result(
+                            [messages_map.get(msg_id) for msg_id in r.ids]
+                        )
                 return
+            except asyncio.CancelledError:
+                pass
+            except Exception as ex:
+                e = ex
+                me = await bot.get_me()
+                logger.error(
+                    f"{me.name} failed to get messages: {ex}, retrying with next bot..."
+                )
+                bot = self.tdlib.next_bot
+        if not e:
+            return
 
-            ids: Set[int] = reduce(
-                lambda full, req: full.union(req.ids), requests, set()
-            )
-            messages = await self.tdlib.next_bot.get_messages(
-                GetMessagesReq(chat=self.private_file_channel, message_ids=tuple(ids))
-            )
-
-            messages_map = {msg.message_id: msg for msg in messages if msg is not None}
-
-            for r in requests:
-                if not r.future.done():
-                    r.future.set_result([messages_map.get(msg_id) for msg_id in r.ids])
-        except asyncio.CancelledError:
-            pass
-        except Exception as e:
-            for request in requests:
-                if not request.future.done():
-                    request.future.set_exception(e)
+        logger.error(
+            "All bots failed to get messages, propagating exception to all requests."
+        )
+        for request in requests:
+            if e and not request.future.done():
+                request.future.set_exception(e)
